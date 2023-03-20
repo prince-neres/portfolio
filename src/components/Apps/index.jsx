72eeb9892b352799f96161e09381639fc822cca2
Prince Neres <prince84224960@gmail.com>
2023-03-20 15:51:47 -0300

    Add multiple previews in hover

--- src/components/Apps/index.jsx
+++ src/components/Apps/index.jsx
@@ -6,7 +6,10 @@ const Apps = () => {
 			<div className='grid grid-cols-1 md:grid-cols-3 gap-5 px-10 md:px-32 lg:px-0'>
 				{projectsJson.map((project, index) =>
 					<div key={index} className='text-center'>
-						<img src={project.banner} className='pb-3' />
+						<div className='group'>
+							<img src={project.previews[0]} className='pb-3 group-hover:hidden' />
+							<img src={project.previews[1] || project.previews[0]} className='pb-3 hidden group-hover:block' />
+						</div>
 						<h3 className='font-bold text-sun'>{project.name}</h3>
 						{project.url ? <a href={project.url} target='_blank' className='block hover:text-sun duration-100'><i>Link</i></a> : null}
 						{project.repo ? <a href={project.repo} target='_blank' className='block hover:text-sun duration-100'><i>Repositório</i></a> : null}
