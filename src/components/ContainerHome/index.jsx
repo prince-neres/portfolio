56318461e4e893630a6610e60672d6633c1ea80a
Prince Neres <prince84224960@gmail.com>
2023-04-08 21:43:56 -0300

    Add spin in loading images

--- src/components/ContainerHome/index.jsx
+++ src/components/ContainerHome/index.jsx
@@ -1,59 +1,66 @@
-import { useState, useContext, useEffect } from 'react';
-import { FaGithub, FaLinkedin } from 'react-icons/fa';
-import { SiGmail } from 'react-icons/si';
-import { RiInstagramFill } from 'react-icons/ri';
-import { IoLogoWhatsapp } from 'react-icons/io';
-import { Context } from '../../Context';
-import about_pt from './about_pt.json';
-import about_en from './about_en.json';
-import Me from '../../assets/me.png';
+import { useState, useContext, useEffect } from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+import { RiInstagramFill } from "react-icons/ri";
+import { IoLogoWhatsapp } from "react-icons/io";
+import { Context } from "../../Context";
+import about_pt from "./about_pt.json";
+import about_en from "./about_en.json";
+import Me from "../../assets/me.png";
 
 const ContainerHome = () => {
-	const [language] = useContext(Context);
-	const defineContent = language === 'pt-BR' ? about_pt : about_en;
-	const [content, setContent] = useState(defineContent);
+  const [language] = useContext(Context);
+  const defineContent = language === "pt-BR" ? about_pt : about_en;
+  const [content, setContent] = useState(defineContent);
 
-	const icons = [
-		<IoLogoWhatsapp className='mr-2' />,
-		<SiGmail className='mr-2' />,
-		<FaLinkedin className='mr-2' />,
-		<FaGithub className='mr-2'/>,
-		<RiInstagramFill className='mr-2' />
-	];
+  const icons = [
+    <IoLogoWhatsapp className="mr-2" />,
+    <SiGmail className="mr-2" />,
+    <FaLinkedin className="mr-2" />,
+    <FaGithub className="mr-2" />,
+    <RiInstagramFill className="mr-2" />,
+  ];
 
-	useEffect(() => {
-		setContent(defineContent);
-	}, [language]);
+  useEffect(() => {
+    setContent(defineContent);
+  }, [language]);
 
-	return (
-		<main className='h-auto p-3 lg:px-56'>
-			<div className='items-center p-5 text-center sm:flex-col md:flex-row lg:px-32 lg:text-left lg:flex'>
-				<img src={Me} className='rounded-full mb-5 sm:w-72 sm:h-72 mx-auto lg:m-0' />
-				<div className='md:pl-10'>
-					<div className='pb-5'>
-						<h1 className='text-3xl font-bold'>{content.title}</h1>
-						<i className='text-sun'>{content.office}</i>
-					</div>
-					{
-						content.paragraphs.map((paragraph, index) =>
-							<p className='indent-8 text-justify' key={index}>
-								{paragraph}
-							</p>
-						)
-					}
-				</div>
-			</div>
-			<div className='w-full text-center'>
-				<ul className='w-full flex flex-wrap justify-center px-10 pb-10'>
-					{
-						content.contacts.map((contact, index) => 
-								<a key={index} href={contact.url} target='_blank' className='contact'>{icons[index]}{contact.name}</a>
-						)
-					}
-				</ul>
-			</div>
-		</main>
-	)
+  return (
+    <main className="h-auto p-3 lg:px-56">
+      <div className="items-center p-5 text-center sm:flex-col md:flex-row lg:px-32 lg:text-left lg:flex">
+        <img
+          src={Me}
+          className="rounded-full mb-5 h-36 w-36 sm:w-72 sm:h-72 mx-auto lg:m-0"
+        />
+        <div className="md:pl-10">
+          <div className="pb-5">
+            <h1 className="text-lg md:text-3xl font-bold">{content.title}</h1>
+            <i className="text-sun">{content.office}</i>
+          </div>
+          {content.paragraphs.map((paragraph, index) => (
+            <p className="indent-8 text-justify" key={index}>
+              {paragraph}
+            </p>
+          ))}
+        </div>
+      </div>
+      <div className="w-full text-center">
+        <ul className="w-full flex flex-wrap justify-center px-10 pb-10">
+          {content.contacts.map((contact, index) => (
+            <a
+              key={index}
+              href={contact.url}
+              target="_blank"
+              className="contact"
+            >
+              {icons[index]}
+              {contact.name}
+            </a>
+          ))}
+        </ul>
+      </div>
+    </main>
+  );
 };
 
 export default ContainerHome;
--- src/components/ContainerProjects/index.jsx
+++ src/components/ContainerProjects/index.jsx
@@ -1,34 +1,66 @@
-import { useContext, useState, useEffect } from 'react';
-import { Context } from '../../Context';
-import projects_en from './projects_en.json';
-import projects_pt from './projects_pt.json';
+import { useContext, useState, useEffect } from "react";
+import Spin from "../Spin";
+import { Context } from "../../Context";
+import projects_en from "./projects_en.json";
+import projects_pt from "./projects_pt.json";
 
 const ContainerProjects = () => {
-	const [language] = useContext(Context);
-	const defineContent = language === 'pt-BR' ? projects_pt : projects_en
-	const [content, setContent] = useState(defineContent)
+  const [language] = useContext(Context);
+  const defineContent = language === "pt-BR" ? projects_pt : projects_en;
+  const [content, setContent] = useState(defineContent);
+  const [imageLoaded, setImageLoaded] = useState(false);
 
-	useEffect(() => {
-		setContent(defineContent)
-	}, [language])
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
 
-	return (
-		<div className='h-auto p-3 lg:px-56'>
-			<div className='grid grid-cols-1 md:grid-cols-3 gap-5 px-10 md:px-32 lg:px-0'>
-				{content.projects.map((project, index) =>
-					<div key={index} className='text-center'>
-						<div className='group'>
-							<img src={project.previews[0]} className='pb-3 group-hover:hidden' />
-							<img src={project.previews[1] || project.previews[0]} className='pb-3 hidden group-hover:block' />
-						</div>
-						<h3 className='font-bold text-sun'>{project.name}</h3>
-						{project.url ? <a href={project.url} target='_blank' className='block hover:text-sun duration-100'><i>{content.titles.link}</i></a> : null}
-						{project.repo ? <a href={project.repo} target='_blank' className='block hover:text-sun duration-100'><i>{content.titles.repo}</i></a> : null}
-					</div>
-				)}
-			</div>
-		</div>
-	)
+  useEffect(() => {
+    setContent(defineContent);
+  }, [language]);
+
+  return (
+    <div className="h-auto p-3 lg:px-56">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5 px-10 md:px-32 lg:px-0">
+        {content.projects.map((project, index) => (
+          <div key={index} className="text-center">
+            <div className="group">
+              {!imageLoaded && <Spin />}
+              <img
+                src={project.previews[0]}
+                className={`pb-3 group-hover:hidden ${
+                  imageLoaded ? "block" : "hidden"
+                }`}
+                onLoad={handleImageLoad}
+              />
+              <img
+                src={project.previews[1] || project.previews[0]}
+                className="pb-3 hidden group-hover:block"
+              />
+            </div>
+            <h3 className="font-bold text-sun">{project.name}</h3>
+            {project.url ? (
+              <a
+                href={project.url}
+                target="_blank"
+                className="block hover:text-sun duration-100"
+              >
+                <i>{content.titles.link}</i>
+              </a>
+            ) : null}
+            {project.repo ? (
+              <a
+                href={project.repo}
+                target="_blank"
+                className="block hover:text-sun duration-100"
+              >
+                <i>{content.titles.repo}</i>
+              </a>
+            ) : null}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ContainerProjects;
