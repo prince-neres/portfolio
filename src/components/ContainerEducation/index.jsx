00c9f6902b4cd33140da21bf0a7dfc44ff5b6ab9
Prince Neres <prince84224960@gmail.com>
2023-04-10 14:57:24 -0300

    Fix tags

--- src/components/ContainerEducation/index.jsx
+++ src/components/ContainerEducation/index.jsx
@@ -1,48 +1,53 @@
-import { formatDate, formatLocation } from '../../commons/utils.js';
-import education_pt from './education_pt.json';
-import education_en from './education_en.json';
-import { useContext, useEffect, useState } from 'react';
-import { Context } from '../../Context';
+import { formatDate, formatLocation } from "../../commons/utils.js";
+import education_pt from "./education_pt.json";
+import education_en from "./education_en.json";
+import { useContext, useEffect, useState } from "react";
+import { Context } from "../../Context";
 
 const ContainerEducation = () => {
-	const [language] = useContext(Context);
-	const defineContent = language === 'pt-BR' ? education_pt : education_en;
-	const [content, setContent] = useState(defineContent);
+  const [language] = useContext(Context);
+  const defineContent = language === "pt-BR" ? education_pt : education_en;
+  const [content, setContent] = useState(defineContent);
 
-	useEffect(() => {
-		setContent(defineContent);
-	}, [language]);
+  useEffect(() => {
+    setContent(defineContent);
+  }, [language]);
 
-	return (
-		<div className='h-auto p-3 lg:px-56'>
-			{content.academic.map((education, index) => (
-				<div key={index} className='px-10 py-4'>
-					<h3 className='font-bold text-xl text-sun'>{education.institute}</h3>
-					<h4 className='font-bold'>{`${education.course} · ${education.type}`}</h4>
-					 <p><i>{formatLocation(education.location)}</i></p>
-					<span>{`${formatDate(education.start_date, language)} - ${formatDate(education.end_date, language)}`}</span>
-					<p className='indent-8 text-justify pt-2'>{education.description}</p>
-					<div className='flex flex-row flex-wrap pt-2 items-center'>
-						<strong>{language === 'pt-BR' ? 'Competências' : 'Skills'}:</strong>
-						{education.teachings.map((teaching, index) =>
-							<div key={index} className='my-3'>
-								<span className='p-2 bg-sun rounded-full text-dark-200 font-fira text-base mx-2'>
-									{`${teaching}`}
-								</span>
-							</div>
-						)}
-					</div>
-				</div>
-			))}
-			<div className='text-center py-10'>
-				<a href={content.certifications.url} target='_blank'>
-	        <button className='font-bold text-dark-200 dark:text-light-100 outline outline-sun p-5 rounded-full hover:p-6 hover:bg-sun hover:dark:text-dark-200 duration-300'>
-						{content.certifications.title}
-					</button>
-				</a>
-			</div>
-		</div>
-	)
+  return (
+    <div className="h-auto p-3 lg:px-56">
+      {content.academic.map((education, index) => (
+        <div key={index} className="px-10 py-4">
+          <h3 className="font-bold text-xl text-sun">{education.institute}</h3>
+          <h4 className="font-bold">{`${education.course} · ${education.type}`}</h4>
+          <p>
+            <i>{formatLocation(education.location)}</i>
+          </p>
+          <span>{`${formatDate(education.start_date, language)} - ${formatDate(
+            education.end_date,
+            language
+          )}`}</span>
+          <p className="indent-8 text-justify pt-2">{education.description}</p>
+          <div className="flex flex-row flex-wrap pt-2 items-center">
+            <strong>{language === "pt-BR" ? "Competências" : "Skills"}:</strong>
+            {education.teachings.map((teaching, index) => (
+              <div key={index} className="my-3">
+                <span className="p-2 text-sm bg-sun rounded-full text-dark-200 font-fira mx-2">
+                  {`${teaching}`}
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+      <div className="text-center py-10">
+        <a href={content.certifications.url} target="_blank">
+          <button className="font-bold text-dark-200 dark:text-light-100 outline outline-sun p-5 rounded-full hover:p-6 hover:bg-sun hover:dark:text-dark-200 duration-300">
+            {content.certifications.title}
+          </button>
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default ContainerEducation;
--- src/components/ContainerExperience/index.jsx
+++ src/components/ContainerExperience/index.jsx
@@ -1,41 +1,46 @@
-import { useContext, useState, useEffect } from 'react';
-import { formatDate, formatLocation, monthsDiff } from '../../commons/utils.js';
-import { Context } from '../../Context.js';
-import xp_pt from './xp_pt.json';
-import xp_en from './xp_en.json';
+import { useContext, useState, useEffect } from "react";
+import { formatDate, formatLocation, monthsDiff } from "../../commons/utils.js";
+import { Context } from "../../Context.js";
+import xp_pt from "./xp_pt.json";
+import xp_en from "./xp_en.json";
 
 const ContainerExperience = () => {
-	const [language] = useContext(Context);
-	const defineContent = language === 'pt-BR' ? xp_pt : xp_en; 
-	const [content, setContent] = useState(defineContent);
+  const [language] = useContext(Context);
+  const defineContent = language === "pt-BR" ? xp_pt : xp_en;
+  const [content, setContent] = useState(defineContent);
 
-	useEffect(() => {
-		setContent(defineContent);
-	}, [language]);
+  useEffect(() => {
+    setContent(defineContent);
+  }, [language]);
 
-	return (
-		<div className='h-auto p-3 lg:px-72'>
-			{content.map((xp, index) => (
-				<div key={index} className='px-10 py-4'>
-					<h3 className='font-bold text-xl text-sun'>{xp.office}</h3>
-					<h4 className='font-bold'>{`${xp.company} · ${xp.type}`}</h4>
-					 <p><i>{formatLocation(xp.location)}</i></p>
-					<span>{`${formatDate(xp.start_date, language)} - ${formatDate(xp.end_date, language)} - ${monthsDiff(xp.start_date, xp.end_date, language)}`}</span>
-					<p className='indent-8 text-justify pt-2'>{xp.description}</p>
-					<div className='flex flex-row flex-wrap pt-2 items-center'>
-						<strong>{language === 'pt-BR' ? 'Competências' : 'Skills'}:</strong>
-						{xp.skills.map((skill, index) =>
-							<div key={index} className='my-3'>
-								<span className='p-2 bg-sun rounded-full text-dark-200 font-fira text-base mx-2'>
-									{`${skill}`}
-								</span>
-							</div>
-						)}
-					</div>
-				</div>
-			))}
-		</div>
-	)
+  return (
+    <div className="h-auto p-3 lg:px-72">
+      {content.map((xp, index) => (
+        <div key={index} className="px-10 py-4">
+          <h3 className="font-bold text-xl text-sun">{xp.office}</h3>
+          <h4 className="font-bold">{`${xp.company} · ${xp.type}`}</h4>
+          <p>
+            <i>{formatLocation(xp.location)}</i>
+          </p>
+          <span>{`${formatDate(xp.start_date, language)} - ${formatDate(
+            xp.end_date,
+            language
+          )} - ${monthsDiff(xp.start_date, xp.end_date, language)}`}</span>
+          <p className="indent-8 text-justify pt-2">{xp.description}</p>
+          <div className="flex flex-row flex-wrap pt-2 items-center">
+            <strong>{language === "pt-BR" ? "Competências" : "Skills"}:</strong>
+            {xp.skills.map((skill, index) => (
+              <div key={index} className="my-3">
+                <span className="p-2 bg-sun rounded-full text-dark-200 font-fira mx-2 text-sm">
+                  {`${skill}`}
+                </span>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ContainerExperience;
