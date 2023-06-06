6c9bcecf392d48ff86fd9183d68c0e4d72374380
Prince Neres <prince84224960@gmail.com>
2023-06-05 21:32:15 -0300

    feat: Add new Job

--- src/components/Experience/index.jsx
+++ src/components/Experience/index.jsx
@@ -30,10 +30,17 @@ const Experience = () => {
           <p>
             <i>{formatLocation(xp.location)}</i>
           </p>
-          <span>{`${formatDate(xp.start_date, language)} - ${formatDate(
-            xp.end_date,
-            language
-          )} - ${monthsDiff(xp.start_date, xp.end_date, language)}`}</span>
+          <span>{`${formatDate(xp.start_date, language)} - ${
+            xp.end_date
+              ? `${formatDate(xp.end_date, language)} - ${monthsDiff(
+                  xp.start_date,
+                  xp.end_date,
+                  language
+                )}`
+              : language === "pt-BR"
+              ? "Presente"
+              : "Present"
+          }`}</span>
           <p className="indent-8 text-justify pt-2">{xp.description}</p>
           <div className="flex flex-row flex-wrap pt-2 items-center">
             <strong className="mr-2">
